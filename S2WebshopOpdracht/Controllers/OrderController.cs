b38035a22066d7afbf6f6cda44812d1db66c644d
Tim S <tim.sanders@student.fontys.nl>
2018-01-03 15:04:21 +0100

    Product view namen aangepast. Reviews toegevoegd aan product controller. Order logic toegevoegd aan ordercontroller.

--- S2WebshopOpdracht/Controllers/OrderController.cs
+++ S2WebshopOpdracht/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Logic;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,33 +10,42 @@ namespace S2WebshopOpdracht.Controllers
 {
     public class OrderController : Controller
     {
+        private OrderLogic orderLogic = new OrderLogic();
+
         // GET: Order
-        public ActionResult Index()
+        public ActionResult IndexOrder()
         {
-            return View();
+            List<Order> orders = orderLogic.GetAllOrders();
+            return View(orders);
         }
 
         // GET: Order/Details/5
-        public ActionResult Details(int id)
+        public ActionResult DetailsOrder(int id)
         {
-            return View();
+            Order order = orderLogic.GetOrderById(id);
+            if (order != null)
+            {
+                return View(order);
+            }
+            else return HttpNotFound();
         }
 
         // GET: Order/Create
-        public ActionResult Create()
+        public ActionResult CreateOrder()
         {
             return View();
         }
 
         // POST: Order/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult CreateOrder(FormCollection collection)
         {
             try
             {
                 // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                Order order = new Order(Convert.ToInt32(collection["AccountID"]), collection["DeliveryName"], collection["BillingName"], false);
+                orderLogic.InsertOrder(order);
+                return RedirectToAction("IndexOrder");
             }
             catch
             {
@@ -43,19 +54,25 @@ namespace S2WebshopOpdracht.Controllers
         }
 
         // GET: Order/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult EditOrder(int id)
         {
-            return View();
+            Order order = orderLogic.GetOrderById(id);
+            if (order != null)
+            {
+                return View(order);
+            }
+            else return HttpNotFound();
         }
 
         // POST: Order/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult EditOrder(int id, FormCollection collection)
         {
             try
             {
                 // TODO: Add update logic here
-
+                Order order = new Order(id, Convert.ToInt32(collection["AccountID"]), collection["DeliveryName"], collection["BillingName"], Convert.ToBoolean(collection["OrderStatus"]));
+                orderLogic.UpdateOrder(order);
                 return RedirectToAction("Index");
             }
             catch
@@ -65,19 +82,24 @@ namespace S2WebshopOpdracht.Controllers
         }
 
         // GET: Order/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult DeleteOrder(int id)
         {
-            return View();
+            Order order = orderLogic.GetOrderById(id);
+            if (order != null)
+            {
+                return View(order);
+            }
+            else return HttpNotFound();
         }
 
         // POST: Order/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult DeleteOrder(int id, FormCollection collection)
         {
             try
             {
                 // TODO: Add delete logic here
-
+                orderLogic.DeleteOrder(id);
                 return RedirectToAction("Index");
             }
             catch
--- S2WebshopOpdracht/Controllers/ProductController.cs
+++ S2WebshopOpdracht/Controllers/ProductController.cs
@@ -11,15 +11,16 @@ namespace S2WebshopOpdracht.Controllers
     public class ProductController : Controller
     {
         private ProductLogic productLogic = new ProductLogic();
+
         // GET: Product
-        public ActionResult Index()
+        public ActionResult IndexProduct()
         {
             List<Product> products = productLogic.GetAllProducts();
             return View(products);
         }
 
         // GET: Product/Details/5
-        public ActionResult Details(int id)
+        public ActionResult DetailsProduct(int id)
         {
             Product product = productLogic.GetProductById(id);
             if (product != null)
@@ -30,21 +31,21 @@ namespace S2WebshopOpdracht.Controllers
         }
 
         // GET: Product/Create
-        public ActionResult Create()
+        public ActionResult CreateProduct()
         {
             return View();
         }
 
         // POST: Product/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult CreateProduct(FormCollection collection)
         {
             try
             {
                 // TODO: Add insert logic here
                 Product product = new Product(Convert.ToInt32(collection["ManufacturerID"]), collection["Name"], collection["Description"], Convert.ToDecimal(collection["Price"]), Convert.ToInt32(collection["Stock"]));
                 productLogic.InsertProduct(product);
-                return RedirectToAction("Index");
+                return RedirectToAction("IndexProduct");
             }
             catch
             {
@@ -53,7 +54,7 @@ namespace S2WebshopOpdracht.Controllers
         }
 
         // GET: Product/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult EditProduct(int id)
         {
             Product product = productLogic.GetProductById(id);
             if (product != null)
@@ -65,14 +66,14 @@ namespace S2WebshopOpdracht.Controllers
 
         // POST: Product/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult EditProduct(int id, FormCollection collection)
         {
             try
             {
                 // TODO: Add update logic here
                 Product product = new Product(id, Convert.ToInt32(collection["ManufacturerID"]), collection["Name"], collection["Description"], Convert.ToDecimal(collection["Price"]), Convert.ToInt32(collection["Stock"]));
                 productLogic.UpdateProduct(product);
-                return RedirectToAction("Index");
+                return RedirectToAction("IndexProduct");
             }
             catch
             {
@@ -81,7 +82,7 @@ namespace S2WebshopOpdracht.Controllers
         }
 
         // GET: Product/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult DeleteProduct(int id)
         {
             Product product = productLogic.GetProductById(id);
             if (product != null)
@@ -93,12 +94,108 @@ namespace S2WebshopOpdracht.Controllers
 
         // POST: Product/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult DeleteProduct(int id, FormCollection collection)
         {
             try
             {
                 // TODO: Add delete logic here
                 productLogic.DeleteProduct(id);
+                return RedirectToAction("IndexProduct");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review
+        public ActionResult IndexReview()
+        {
+            List<Review> reviews = productLogic.GetAllReviews();
+            return View(reviews);
+        }
+
+        // GET: Review/Details/5
+        public ActionResult DetailsReview(int id)
+        {
+            Review review = productLogic.GetReviewById(id);
+            if (review != null)
+            {
+                return View(review);
+            }
+            else return HttpNotFound();
+        }
+
+        // GET: Review/Create
+        public ActionResult CreateReview()
+        {
+            return View();
+        }
+
+        // POST: Review/Create
+        [HttpPost]
+        public ActionResult CreateReview(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Review review = new Review(Convert.ToInt32(collection["AccountID"]), Convert.ToInt32(collection["ProductID"]), collection["Rating"], collection["ReviewText"]);
+                productLogic.InsertReview(review);
+                return RedirectToAction("IndexReview");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Edit/5
+        public ActionResult EditReview(int id)
+        {
+            Review review = productLogic.GetReviewById(id);
+            if (review != null)
+            {
+                return View(review);
+            }
+            else return HttpNotFound();
+        }
+
+        // POST: Review/Edit/5
+        [HttpPost]
+        public ActionResult EditReview(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Review review = new Review(id, Convert.ToInt32(collection["AccountID"]), Convert.ToInt32(collection["ProductID"]), collection["Rating"], collection["ReviewText"]);
+                productLogic.UpdateReview(review);
+                return RedirectToAction("IndexReview");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Review/Delete/5
+        public ActionResult DeleteReview(int id)
+        {
+            Review review = productLogic.GetReviewById(id);
+            if (review != null)
+            {
+                return View(review);
+            }
+            else return HttpNotFound();
+        }
+
+        // POST: Review/Delete/5
+        [HttpPost]
+        public ActionResult DeleteReview(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                productLogic.DeleteReview(id);
                 return RedirectToAction("Index");
             }
             catch
