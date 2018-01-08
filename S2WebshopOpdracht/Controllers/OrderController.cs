7ed396bdddf0ecc788a3ad4133c658ae6522b429
Tim S <tim.sanders@student.fontys.nl>
2018-01-08 10:16:15 +0100

    OrderedProduct toegevoegd aan controller

--- S2WebshopOpdracht/Controllers/OrderController.cs
+++ S2WebshopOpdracht/Controllers/OrderController.cs
@@ -107,5 +107,101 @@ namespace S2WebshopOpdracht.Controllers
                 return View();
             }
         }
+
+        // GET: Order
+        public ActionResult IndexOrderedProduct()
+        {
+            List<OrderedProduct> orderedproducts = orderLogic.GetAllOrderedProducts();
+            return View(orderedproducts);
+        }
+
+        // GET: Order/Details/5
+        public ActionResult DetailsOrderedProduct(int id)
+        {
+            OrderedProduct orderedproduct = orderLogic.GetOrderedProductById(id);
+            if (orderedproduct != null)
+            {
+                return View(orderedproduct);
+            }
+            else return HttpNotFound();
+        }
+
+        // GET: Order/Create
+        public ActionResult CreateOrderedProduct()
+        {
+            return View();
+        }
+
+        // POST: Order/Create
+        [HttpPost]
+        public ActionResult CreateOrderedProduct(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                OrderedProduct orderedproduct = new OrderedProduct(Convert.ToInt32(collection["OrderID"]), Convert.ToInt32(collection["ProductID"]), Convert.ToInt32(collection["Quantity"]));
+                orderLogic.InsertOrderedProduct(orderedproduct);
+                return RedirectToAction("IndexOrderedProduct");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult EditOrdered(int id)
+        {
+            OrderedProduct orderedproduct = orderLogic.GetOrderedProductById(id);
+            if (orderedproduct != null)
+            {
+                return View(orderedproduct);
+            }
+            else return HttpNotFound();
+        }
+
+        // POST: Order/Edit/5
+        [HttpPost]
+        public ActionResult EditOrderedProduct(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                OrderedProduct orderedproduct = new OrderedProduct(id, Convert.ToInt32(collection["OrderID"]), Convert.ToInt32(collection["ProductID"]), Convert.ToInt32(collection["Quantity"]));
+                orderLogic.UpdateOrderedProduct(orderedproduct);
+                return RedirectToAction("IndexOrderedProduct");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult DeleteOrderedProduct(int id)
+        {
+            OrderedProduct orderedproduct = orderLogic.GetOrderedProductById(id);
+            if (orderedproduct != null)
+            {
+                return View(orderedproduct);
+            }
+            else return HttpNotFound();
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost]
+        public ActionResult DeleteOrderedProduct(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                orderLogic.DeleteOrderedProduct(id);
+                return RedirectToAction("IndexOrderedProduct");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
