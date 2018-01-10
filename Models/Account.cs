efccc8fae4c4929f48b46cca55d5203aa00ae27a
Tim S <tim.sanders@student.fontys.nl>
2018-01-10 11:01:29 +0100

    changes to account model

--- Models/Account.cs
+++ Models/Account.cs
@@ -9,31 +9,18 @@ namespace Models
     {
         private int id;
         private int addressid;
-        private Customer customer;
-        private Administrator administrator;
         private string username;
         private string password;
         private string email;
 
         public int Id { get { return id; } set { id = value; } }
         public int AddressId { get { return addressid; } set { addressid = value; } }
-        public Customer Customer { get { return customer; } set { customer = value; } }
-        public Administrator Administrator { get { return administrator; } set { administrator = value; } }
         public string Username { get { return username; } set { username = value; } }
         public string Password { get { return password; } set { password = value; } }
         public string Email { get { return email; } set { email = value; } }
 
-        public Account(int id, int addressid, Customer customer, string username, string password, string email)
-           : this(id, addressid, username, password, email)
-        {
-            this.customer = customer;
-        }
 
-        public Account(int id, int addressid, Administrator administrator, string username, string password, string email)
-            :this(id, addressid, username, password, email)
-        {
-            this.administrator = administrator;
-        }
+
 
         private Account(int id, int addressid, string username, string password, string email)
         {
---
+++ S2WebshopOpdracht/ViewModels/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace S2WebshopOpdracht.ViewModels
+{
+    public class OrderViewModel
+    {
+        public string DeliveryName { get; set; }
+        public string BillingName { get; set; }
+        public string Username { get; set; }
+        public bool OrderStatus { get; set; }
+    }
+}
